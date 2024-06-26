acd02df29e0a2f385ec421d7183b9fd656265dd1
Sreekar Hari Kopanathi <v-skopanathi@expediagroup.com>
2024-06-26 15:00:16 +0530

    adding admin controller

---
+++ src/main/java/com/restapi/AdminsController.java
@@ -0,0 +1,32 @@
+package com.restapi;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admins")
+public class AdminsController {
+
+    @GetMapping
+    public String getAdmin(){
+        return "http GET request to get the admin details";
+    }
+
+    @GetMapping(path = "/{adminId}")
+    public String getAdminId(@PathVariable String adminId){
+        return "GET request to get the adminId " + adminId ;
+    }
+
+    @PostMapping
+    public String createAdmin(){
+        return "http POST request to create the admin";
+    }
+    @PutMapping
+    public String updateAdmin(){
+        return "http GET request to update the admin details";
+    }
+    @DeleteMapping
+    public String deleteAdmin(){
+        return "http GET request to delete the admin";
+    }
+
+}
