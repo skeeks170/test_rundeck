0fd0837022991ef57da5d3ca5744883aad1fe932
Sreekar K Hari <92702418+skeeks170@users.noreply.github.com>
2024-06-28 13:55:56 +0530

    test 002

--- src/main/java/com/restapi/AdminsController.java
+++ src/main/java/com/restapi/AdminsController.java
@@ -3,10 +3,9 @@ package com.restapi;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/admins")
 public class AdminsController {
 
-    @GetMapping
+    @GetMapping(path = "admin")
     public String getAdmin(){
         return "http GET request to get the admin details";
     }
@@ -16,15 +15,15 @@ public class AdminsController {
         return "GET request to get the adminId " + adminId ;
     }
 
-    @PostMapping
+    @PostMapping(path = "create")
     public String createAdmin(){
         return "http POST request to create the admin";
     }
-    @PutMapping
+    @PutMapping(path = "update")
     public String updateAdmin(){
         return "http GET request to update the admin details";
     }
-    @DeleteMapping
+    @DeleteMapping(path = "delete")
     public String deleteAdmin(){
         return "http GET request to delete the admin";
     }
